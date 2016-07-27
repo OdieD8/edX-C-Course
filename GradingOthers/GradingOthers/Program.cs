0bc8c51e6c3024246ef5ec033d5d7fab38552509
OdieD8 <odie_de_moura@hotmail.com>
2016-07-26 21:47:06 -0600

    First commit

---
+++ GradingOthers/GradingOthers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sFirst;
+            string sLast;
+            string sAddress1;
+            string sAddress2;
+            string sCity;
+            string sState;
+            int sZip;
+            string sCountry;
+            string sBirthdate;
+
+            //I know i could assign the values above instead. I decided to take the time to type it out.
+            sFirst = "Sven";
+            sLast = "Drogon";
+            sAddress1 = "123 Elm Street";
+            sAddress2 = "Suite 501";
+            sCity = "Atlanta";
+            sState = "GA";
+            sZip = 30008;
+            sCountry = "USA";
+            sBirthdate = "03/05/1985";
+
+            Console.WriteLine("Fisrt name: " + sFirst + " Last name: " + sLast);
+            Console.WriteLine("Students Birthday: " + sBirthdate);
+            Console.WriteLine("Address Line 1: " + sAddress1);
+            Console.WriteLine("Address line 2: " + sAddress2);
+            Console.WriteLine("City, State Zipcode country: " + sCity + ", " + sState + " " + sZip + " " + sCountry);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ModuleOneAssignment/ModuleOneAssignment/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleOneAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string birthDate;
+            string addressLine1;
+            string addressLine2;
+            string city;
+            string state;
+            int zip;
+            string country;
+
+            firstName = "Joseph";
+            lastName = "Doe";
+            birthDate = "Feb 10 1900";
+            addressLine1 = "122 Anywhere Street";
+            addressLine2 = "Suite 500";
+            city = "New York";
+            state = "New York";
+            zip = 10001;
+            country = "US";
+
+            DateTime myBirthDate = DateTime.Parse(birthDate);
+
+            Console.WriteLine("{0} {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}", firstName, lastName, myBirthDate.ToString("MM/dd/yyyy"), addressLine1, addressLine2, city, state, zip, country);
+
+            Console.WriteLine("Now press any key to enter your own student information... ");
+            Console.ReadLine();
+
+            Console.WriteLine("First Name");
+            string userFirstName = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Last Name");
+            string userLastName = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Birth Date");
+            string userBirthDate = Console.ReadLine();
+
+            DateTime userBirthDay = DateTime.Parse(userBirthDate);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Address Line 1");
+            string userAddressLine1 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Address Line 2 (Enter for none)");
+            string userAddressLine2 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("City");
+            string userCity = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("State");
+            string userState = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Zip Code");
+            string userZip = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Country");
+            string userCountry = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("{0} {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}", userFirstName, userLastName, userBirthDay.ToString("MM/dd/yyyy"), userAddressLine1, userAddressLine2, userCity, userState, userZip, userCountry);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
