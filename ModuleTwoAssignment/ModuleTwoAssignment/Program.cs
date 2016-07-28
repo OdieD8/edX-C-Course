bb0eb7de1811e7d42cdc4c0126eda18271d59098
OdieD8 <odie_de_moura@hotmail.com>
2016-07-27 23:34:37 -0600

    Module 2 completed

---
+++ ModuleTwoAssignment/ModuleTwoAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTwoAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    if(i % 2 > 0)
+                    {
+                        Console.Write("O");
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("X");
+                        Console.Write("O");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
