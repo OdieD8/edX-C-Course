2c615fa5c7dc140d5c8dec23fbe6b01f00549495
OdieD8 <odie_de_moura@hotmail.com>
2016-09-22 22:14:12 -0600

    Enums, structs, and arrays practice

--- GradingOthers/GradingOthers/Program.cs
+++ GradingOthers/GradingOthers/Program.cs
@@ -4,28 +4,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GradingOthers
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 8; i++)
-            {
-                bool isEven = i % 2 == 0 ? true : false;
-                switch (isEven)
-                {
-                    case true:
-                        Console.WriteLine("XOXOXOXO");
-                        break;
-                    case false:
-                        Console.WriteLine("OXOXOXOX");
-                        break;
-                    default:
-                        break;
-                }
-            }
-            Console.ReadLine();
+            //read
+            Console.WriteLine("Please enter student information");
+            StudentInformation student = GetStudentInformation();
+
+            //write
+            Console.WriteLine("Student info gathered.");
+            PrintStudentDetails(student);
+            Console.WriteLine("####");
+
+            //read
+            Console.WriteLine("Please enter teacher information");
+            TeacherInformation teacher = GetTeacherInformation();
+
+            //write
+            Console.WriteLine("Teacher info gathered.");
+            PrintTeacherDetails(teacher);
+            Console.WriteLine("####");
+
+            //wait for Espace
+            Console.ReadKey();
         }
+
+        static StudentInformation GetStudentInformation()
+        {
+            StudentInformation student = new StudentInformation();
+            Console.Write("First Name: ");
+            student.FirstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            student.LastName = Console.ReadLine();
+            Console.Write("Birthdate: ");
+            student.Birthdate = readDateTime();
+            return student;
+        }
+
+        static DateTime readDateTime()
+        {
+            try
+            {
+                return DateTime.Parse(Console.ReadLine());
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine("Wrong Format! Please enter correct date.");
+                return readDateTime();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Date not entered correctly! Please enter correct date.");
+                return readDateTime();
+            }
+        }
+
+        static void PrintStudentDetails(StudentInformation student)
+        {
+            Console.WriteLine("First Name: {0}\r\nLast Name: {1}\r\nBirthdate: {2}",
+            student.FirstName, student.LastName, student.Birthdate);
+        }
+
+        static TeacherInformation GetTeacherInformation()
+        {
+            TeacherInformation teacher = new TeacherInformation();
+            Console.Write("Address Line 1: ");
+            teacher.AddressLine1 = Console.ReadLine();
+            Console.Write("Address Line 2: ");
+            teacher.AddressLine2 = Console.ReadLine();
+            Console.Write("City: ");
+            teacher.City = Console.ReadLine();
+            return teacher;
+        }
+
+        static void PrintTeacherDetails(TeacherInformation teacher)
+        {
+            Console.WriteLine("Address Line 1: {0}\r\nAddress Line 2: {1}\r\nCity: {2}",
+            teacher.AddressLine1, teacher.AddressLine2, teacher.City);
+        }
+    }
+
+    class StudentInformation
+    {
+        public string FirstName;
+        public string LastName;
+        public DateTime Birthdate;
+        public string AddressLine1;
+        public string AddressLine2;
+        public string City;
+        public string State_Province;
+        public string Zip_Postal;
+        public string Country;
+    }
+
+    class TeacherInformation : StudentInformation { }
+
+    class UProgramInformation
+    {
+        string ProgramName;
+        string DepartmentHead;
+        string[] Degrees;
+    }
+
+    class DegreeInformation
+    {
+        string DegreeName;
+        Boolean CreditsRequired;
+    }
+
+    class CourseInformation
+    {
+        string CourseName;
+        string Credits;
+        int DurationInWeeks;
+        string Teacher;
     }
 }
---
+++ structs-e-enums/structs-e-enums/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structs_e_enums
+{
+    class Program
+    {
+        public struct Student
+        {
+            public string name;
+            public string major;
+            public int age;
+        }
+        public enum Days
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        };
+
+        public enum Months
+        {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        };
+
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.name = "Joe";
+            student.major = "Science";
+            student.age = 22;
+
+            Console.WriteLine("Student's Information:");
+            Console.WriteLine("Name: {0}, Major: {1}, Age: {2}", student.name, student.major, student.age);
+
+            Days churchDay = Days.Sunday;
+            Console.WriteLine("The day to go to church is: " + churchDay);
+
+            Months birthdayMonth = Months.March;
+            Console.WriteLine("My birthday month is: " + birthdayMonth);
+
+            int[] intArray = new int[5];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = i;
+            }
+
+            int counter = 0;
+            while (counter < intArray.Length)
+            {
+                Console.WriteLine(intArray[counter]);
+                counter++;
+            }
+
+            string[] weekDays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            Console.WriteLine("Today is: " + weekDays[4]);
+        }
+    }
+}
