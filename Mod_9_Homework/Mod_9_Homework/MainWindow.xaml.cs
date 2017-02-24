5aa9982f45a35225a6e6490258d3f22f58644f06
OdieD8 <odie_de_moura@hotmail.com>
2017-02-24 16:59:30 -0700

    Module 9 assignment complete

---
+++ Mod_9_Homework/Mod_9_Homework/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mod_9_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            setEnabled();
+        }
+
+        private void setEnabled()
+        {
+            btnNext.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+        }
+
+        List<Student> studentList = new List<Student>();
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtFirstName.Text) || !string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                Student student = new Student();
+                student.FirstName = txtFirstName.Text;
+                student.LastName = txtLastName.Text;
+                student.City = txtCity.Text;
+                studentList.Add(student);
+
+                txtFirstName.Text = "";
+                txtLastName.Text = "";
+                txtCity.Text = "";
+
+                MessageBox.Show("Student Created");
+                if (studentList.Count > 0)
+                {
+                    btnNext.IsEnabled = true;
+                    btnPrevious.IsEnabled = true;
+                }
+                else
+                {
+                    btnNext.IsEnabled = false;
+                    btnPrevious.IsEnabled = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must enter at least a student's first or last name");
+            }
+        }
+
+        private int nextClickCount = -1;
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            nextClickCount++;
+
+            if (nextClickCount >= studentList.Count)
+            {
+                nextClickCount = 0;
+            }
+
+            txtFirstName.Text = studentList[nextClickCount].FirstName;
+            txtLastName.Text = studentList[nextClickCount].LastName;
+            txtCity.Text = studentList[nextClickCount].City;
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            nextClickCount--;
+
+            if (nextClickCount < 0)
+            {
+                nextClickCount = studentList.Count - 1;
+            }
+
+            txtFirstName.Text = studentList[nextClickCount].FirstName;
+            txtLastName.Text = studentList[nextClickCount].LastName;
+            txtCity.Text = studentList[nextClickCount].City;
+        }
+    }
+}
---
+++ Mod_9_Homework/Mod_9_Homework/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    class Student : Person
+    {
+        private string addressLine1, addressLine2, city, state, country;
+        private int zip;
+        public static int count = 0;
+        public Stack<int> grades = new Stack<int>();
+
+        public Student()
+        {
+            count = ++count;
+        }
+
+        public Student(string firstName, string lastName, DateTime birthDay)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDay;
+        }
+
+        public string AddressLine1
+        {
+            get { return addressLine1; }
+            set { addressLine1 = value; }
+        }
+
+        public string AddressLine2
+        {
+            get { return addressLine2; }
+            set { addressLine2 = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public int Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        public void ListGrades()
+        {
+            Console.WriteLine("Grades: ");
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
